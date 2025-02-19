9d4cc54864dbd04a7ccf44df7ffe65ce2d766d99
tito2point0 <defconx00@gmail.com>
2025-02-19 11:49:24 -0500

    refactor Navbar component to simplify structure and improve styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
   
     
 return (
-  <div className="fixed top-0 left-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg shadow-lg">
-  <div className="border-b border-white w-full"></div>
+  <div className="fixed top-0 left-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white">
+  {/* <div className="border-b border-white w-full"></div> */}
   <div className="max-w-5xl mx-auto px-4">
     <div className="flex justify-between items-center h-16">
       <a href="#home" className="font-mono text-xl font-bold text-white">

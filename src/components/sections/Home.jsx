cc51828850409d0d0f667a458f8d5350284d305e
tito2point0 <defconx00@gmail.com>
2025-02-27 16:54:01 -0500

    add Home component for introductory section and update App layout for improved structure

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,16 @@
+export const home = () => {
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <div className="text-center z-10 px-4">
+              <h1>
+                  Hi,  My name is <span className="text-green-700">Jason</span>. 
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default home;

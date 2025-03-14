a7ba541477d1963289c8cd5d956aecb341468b8b
tito2point0 <defconx00@gmail.com>
2025-03-14 18:05:21 -0400

    add RevealOnScroll component; implement intersection observer for scroll-based visibility effects

---
+++ src/components/RevealOnScroll.jsx
@@ -0,0 +1,33 @@
+import { useRef, useEffect} from 'react';
+import PropTypes from 'prop-types';
+
+
+
+export const RevealOnScroll = ({ children }) => {
+    const ref = useRef
+
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting) {
+                ref.current.classList.add("visible")
+            }
+        
+        }, { threshold: 0.2, rootMargin: "0px 0px -50px 0px" })
+        
+        if (ref.current) observer.observe(ref.current);
+
+        return () => observer.disconnect()
+})
+
+  return (
+    <div  ref={ref} className="reveal">
+      {children}
+    </div>
+  );
+};
+RevealOnScroll.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default RevealOnScroll;
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,4 +1,4 @@
-import { ColourfulText } from "../ui/colorful-text";
+
 
 export const Home = () => {
   return (

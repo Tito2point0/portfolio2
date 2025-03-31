9855d203700dfaf18e94e33ad0af3e382184f91c
tito2point0 <defconx00@gmail.com>
2025-03-30 21:39:38 -0400

    feat: update email input handling in ContactMe component; streamline message input state management

--- src/components/sections/ContactMe.jsx
+++ src/components/sections/ContactMe.jsx
@@ -60,6 +60,9 @@ export const ContactMe = () => {
                 name="email"
                 required
                 value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline--none focus:border-blue focus:bg-blue-500/5"
                 placeholder="example@gmail.com"
               />
@@ -70,9 +73,6 @@ export const ContactMe = () => {
                 name="message"
                 required
                 value={formData.message}
-                onChange={(e) =>
-                  setFormData({ ...formData, message: e.target.value })
-                }
                 rows={6}
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline--none focus:border-blue focus:bg-blue-500/5"
                 placeholder="Your message.."
